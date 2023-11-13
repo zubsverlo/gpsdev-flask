be0efc391652ce213ed395bae02ff3091761bc9f
zubsverlo <zub.sverlo@gmail.com>
2023-11-13 14:28:12 +0300

    change frequency to accept a list instead of sep objects

--- gpsdev_flask/api/frequency.py
+++ gpsdev_flask/api/frequency.py
@@ -14,18 +14,27 @@ frequency = Blueprint('frequency', __name__)
 @frequency.route('/', methods=['POST'])
 @api_login_required
 def frequency_post():
-    schema = FrequencySchema()
+    schema = FrequencySchema(many=True)
     try:
-        obj = schema.load(request.get_json())
+        objects = schema.load(request.get_json())
     except ValidationError as e:
         return validation_error_422(e.messages)
-
-    sql = (
-        f"replace into frequency"
-        f"(division_id, employee_id, object_id, frequency) "
-        f"values('{obj['division_id']}', '{obj['employee_id']}', "
-        f"'{obj['object_id']}', '{obj['frequency']}')"
-    )
-
-    db_session.execute(text(sql))
+    print(objects)
+    for obj in objects:
+        if obj['frequency']:
+            sql = (
+                f"replace into frequency"
+                f"(division_id, employee_id, object_id, frequency) "
+                f"values('{obj['division_id']}', '{obj['employee_id']}', "
+                f"'{obj['object_id']}', '{obj['frequency']}')"
+            )
+        else:
+            sql = (
+                f"delete from frequency "
+                f"where division_id = {obj['division_id']} and "
+                f"employee_id = {obj['employee_id']} and "
+                f"object_id = {obj['object_id']}"
+            )
+        db_session.execute(text(sql))
+    db_session.commit()
     return jsonify({}), 201
