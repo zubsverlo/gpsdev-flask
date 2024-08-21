3cc2b3621056367b7ac5c75d1294359fd0685f61
zubsverlo <zub.sverlo@gmail.com>
2024-08-21 13:40:36 +0300

    forbid statements by dates and divisions feature

--- gpsdev_flask/api/statements.py
+++ gpsdev_flask/api/statements.py
@@ -1,38 +1,37 @@
-from flask import Blueprint
-from flask import request, jsonify, g
-from flask_login import current_user
-from marshmallow import ValidationError
-from gpsdev_flask.ma_schemas import StatementsSchema
-from gpsdev_flask.api.error_responses import validation_error_422
-from gpsdev_flask.api import api_login_required
-from gpsdev_flask import redis_session
-from sqlalchemy import select
-from sqlalchemy.sql.expression import func
-from gpsdev_flask import db_session
-from gpsdev_flask.models import PermitStatements
 import json
 
+from flask import Blueprint, g, jsonify, request
+from flask_login import current_user
+from marshmallow import ValidationError
+from sqlalchemy import select
+from sqlalchemy.sql.expression import func
 
-statements = Blueprint('statements', __name__)
+from gpsdev_flask import db_session, redis_session
+from gpsdev_flask.api import api_login_required
+from gpsdev_flask.api.error_responses import validation_error_422
+from gpsdev_flask.ma_schemas import StatementsSchema
+from gpsdev_flask.models import PermitStatements
+import datetime as dt
+
+statements = Blueprint("statements", __name__)
 
 
 def get_permit_statements() -> dict[str, str]:
-    permit_statements = redis_session.get('permit_statements')
+    permit_statements = redis_session.get("permit_statements")
     if not permit_statements:
         sel = select(
             PermitStatements.date,
-            func.json_arrayagg(PermitStatements.object_id).label('array')
+            func.json_arrayagg(PermitStatements.object_id).label("array"),
         ).group_by(PermitStatements.date)
         permit_statements = {
-            str(i.date): i.array
-            for i in db_session.execute(sel).all()
+            str(i.date): i.array for i in db_session.execute(sel).all()
         }
-        redis_session.set('permit_statements', json.dumps(permit_statements))
+        redis_session.set("permit_statements", json.dumps(permit_statements))
         return permit_statements
     return json.loads(permit_statements)
 
 
-@statements.route('/', methods=['POST'])
+@statements.route("/", methods=["POST"])
 @api_login_required
 def statements_main():
     g.loaded_accessed_divisions = current_user.access_list
@@ -46,32 +45,56 @@ def statements_main():
     # к ответу будет добавлено сообщение для уведомления
     permits_notify = False
 
+    # Запрет проставлять выходы до определенной даты,
+    # по определенным подразделениям, за исключением определенных пользователей
+    forbid_notify = False
+    forbid_date_start = dt.date(2024, 8, 1)
+    forbid_date_end = dt.date(2024, 8, 23)
+    forbid_user_exception_ids = tuple((1, ))
+    forbid_division_ids = (1, 2, 7, 8, 12)
+
     for stmt in stmts:
-        if stmt.get('value'):
+        # Здесь прописан запрет проставлять выходы
+        # if (
+        #     stmt["division"] in forbid_division_ids
+        #     and stmt["date"] <= forbid_date_end
+        #     and stmt["date"] >= forbid_date_start
+        #     and current_user.id not in forbid_user_exception_ids
+        # ):
+        #     forbid_notify = True
+        #     continue
+        if stmt.get("value"):
             line = (
                 f"replace into statements_site"
                 f"(division, name_id, object_id, date, statement) "
                 f"values('{stmt['division']}', '{stmt['name_id']}', "
                 f"'{stmt['object_id']}', '{stmt['date']}', '{stmt['value']}')"
             )
-            if stmt['object_id'] in permits.get(str(stmt['date']), []):
+            if stmt["object_id"] in permits.get(str(stmt["date"]), []):
                 permits_notify = True
                 continue
-            redis_session.lpush('queue_sql', line)
+            redis_session.lpush("queue_sql", line)
         else:
             line = (
                 f"delete from statements_site where "
                 f"division = '{stmt['division']}' "
                 f"and name_id = '{stmt['name_id']}' "
                 f"and object_id = '{stmt['object_id']}' "
-                f"and date = '{stmt['date']}'")
-            redis_session.lpush('queue_sql', line)
+                f"and date = '{stmt['date']}'"
+            )
+            redis_session.lpush("queue_sql", line)
 
-    if permits_notify:
-        return jsonify(
-            {
-                'message': ('Некоторые выходы не сохранены '
-                            'из-за запрета руководителя.')
-            }
-        ), 201
+    if permits_notify or forbid_notify:
+        return (
+            jsonify(
+                {
+                    "message": (
+                        "Некоторые выходы не сохранены "
+                        "из-за запрета руководителя."
+                    )
+                }
+            ),
+            201,
+        )
+
     return jsonify({}), 201
