500d32d7166dd90cd9a3369615880cb522a78973
zubsverlo <zub.sverlo@gmail.com>
2024-12-11 11:31:19 +0300

    change forbid dates, divisions

--- gpsdev_flask/api/statements.py
+++ gpsdev_flask/api/statements.py
@@ -48,10 +48,10 @@ def statements_main():
     # Запрет проставлять выходы до определенной даты,
     # по определенным подразделениям, за исключением определенных пользователей
     forbid_notify = False
-    forbid_date_start = dt.date(2024, 11, 1)
-    forbid_date_end = dt.date(2024, 11, 30)
+    forbid_date_start = dt.date(2024, 12, 1)
+    forbid_date_end = dt.date(2024, 12, 8)
     forbid_user_exception_ids = (1, 52, 12)
-    forbid_division_ids = (1, 2, 7, 8, 12, 13, 14)
+    forbid_division_ids = (1, 7, 12, 8, 14)
 
     for stmt in stmts:
         # Здесь прописан запрет проставлять выходы
