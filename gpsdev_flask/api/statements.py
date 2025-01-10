b7d2d00cb7d8cb5ae6f8c8d91f0c0a965ad8f1ab
zubsverlo <zub.sverlo@gmail.com>
2025-01-11 00:45:19 +0300

    update statements forbid rules

--- gpsdev_flask/api/statements.py
+++ gpsdev_flask/api/statements.py
@@ -48,8 +48,8 @@ def statements_main():
     # Запрет проставлять выходы до определенной даты,
     # по определенным подразделениям, за исключением определенных пользователей
     forbid_notify = False
-    forbid_date_start = dt.date(2024, 12, 1)
-    forbid_date_end = dt.date(2024, 12, 15)
+    forbid_date_start = dt.date(2025, 1, 1)
+    forbid_date_end = dt.date(2025, 1, 10)
     forbid_user_exception_ids = (1, 52, 12)
     forbid_division_ids = (1, 2, 7, 8, 12, 13, 14)
 
